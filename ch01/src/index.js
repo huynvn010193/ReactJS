6d6dfd2def10053d73456d55e177e204fb85bf0b
huynvn010193 <hpn010193@gmail.com>
2018-01-31 23:26:33 +0700

    Done clip Bài 7 Tìm hiểu ReactJS phần 4: Tìm hiểu cách tạo component

--- ch01/src/index.js
+++ ch01/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<App />, 
+	<Hello />, 
 	document.getElementById('root'));
 registerServiceWorker();
