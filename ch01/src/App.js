96b16e424f86737ccb17b10d87d763e57ecafc12
huynvn010193 <hpn010193@gmail.com>
2018-02-02 00:15:34 +0700

    Done Bài 14 Sử dụng Props - Phần 2

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       	<div className="row">
-      		<Course />
-      		<Course />
-      		<Course />
+      		<Course name="ReactJS" time="30h" free={true}>ReactJS is very simple</Course>
+      		<Course name="Angular 4x" time="55h" free={false}></Course>
+      		<Course name="NodeJS" time="35h" free={true}></Course>
 		</div>
     );
   }
--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -3,25 +3,30 @@ import Lesson from './Lesson';
 
 class Course extends Component {
     // Viết theo kiểu ES6 => ko cần khai báo kiểu function
-    formatName(user) {
+    /*formatName(user) {
         return user.firstName + ' ' + user.lastName;
+    }*/
+    showButtonFree()
+    {
+        const isFree = this.props.free;
+        if(isFree === true)
+        {
+            return <div className="panel-footer">
+                        <button type="button" className="btn btn-warning">view</button>
+                    </div>;
+        }
     }
-
     render() {
-        const user = {
-            firstName: 'Harper',
-            lastName: 'Perez'
-        };
-
         return (
             <div>
                 <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                     <div className="panel panel-info">
                         <div className="panel-heading">
-                            <h3 className="panel-title">ReactJS</h3>
+                            <h3 className="panel-title">{this.props.name}</h3>
                         </div>
                         <div className="panel-body">
-                            {this.formatName(user)}
+                            <p>{this.props.time}</p>
+                            <p>{this.props.children}</p>
                             <ul className="list-group">
                                 <Lesson />
                                 <Lesson />
@@ -29,12 +34,11 @@ class Course extends Component {
                                 <Lesson />
                             </ul>
                         </div>
+                        {this.showButtonFree()}
                     </div>
                 </div>
-                <button type="button" className="btn btn-success">register</button>
             </div>
         );
-  }
+    }
 }
-
 export default Course;
