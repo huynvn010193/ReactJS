dc94de77cba46399385b209daf4ce1a0e1ed0ecc
HuyNVN <hpn010193@gmail.com>
2018-02-01 20:08:51 +0700

    Done clip Bài 12 Tìm hiểu JSX

--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 import Lesson from './Lesson';
 
 class Course extends Component {
-  render() {
-    return (
-        <div>
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <div className="panel panel-info">
-                    <div className="panel-heading">
-                        <h3 className="panel-title">ReactJS</h3>
-                    </div>
-                    <div className="panel-body">
-                        <ul className="list-group">
-                            <Lesson />
-                            <Lesson />
-                            <Lesson />
-                            <Lesson />
-                        </ul>
+    // Viết theo kiểu ES6 => ko cần khai báo kiểu function
+    formatName(user) {
+        return user.firstName + ' ' + user.lastName;
+    }
+
+    render() {
+        const user = {
+            firstName: 'Harper',
+            lastName: 'Perez'
+        };
+
+        return (
+            <div>
+                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div className="panel panel-info">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">ReactJS</h3>
+                        </div>
+                        <div className="panel-body">
+                            {this.formatName(user)}
+                            <ul className="list-group">
+                                <Lesson />
+                                <Lesson />
+                                <Lesson />
+                                <Lesson />
+                            </ul>
+                        </div>
                     </div>
                 </div>
+                <button type="button" className="btn btn-success">register</button>
             </div>
-            <button type="button" className="btn btn-success">register</button>
-        </div>
-    );
+        );
   }
 }
 
