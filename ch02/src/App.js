a2019d966eaa25d872c3c051339313a85952f198
huynvn010193 <hpn010193@gmail.com>
2018-02-05 23:45:13 +0700

    Done clip Bài 26 Phân chia Component - Phần 2

--- ch02/src/App.js
+++ ch02/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Title from './components/Title';
 import Control from './components/Control';
+import Form from './components/Form';
+import List from './components/List';
 
 class App extends Component {
 	constructor(props)
@@ -12,7 +14,7 @@ class App extends Component {
 	}
 	render() {
 	    return (
-	    <div className="row">
+	    <div>
 	    	{ /*TITLE : START*/ }
 	    	<Title />
 	    	{ /*TITLE : END */ }
@@ -22,70 +24,12 @@ class App extends Component {
 			{/* CONTROL (SEARCH + SORT + ADD) : END */}
 
         	{/* FORM : START */}
-	        <div className="row">
-	          	<div className="col-md-offset-7 col-md-5">
-		            <form action="" method="POST" className="form-inline">
-		              	<div className="form-group">
-			                <label className="sr-only" htmlFor="true">label</label>
-			                <input type="text" className="form-control" placeholder="Task Name" ref="task_name" />
-		              	</div>
-		              	<div className="form-group">
-			                <label className="sr-only" htmlFor="true">label</label>
-			                <select name="ds" id="inputDs" className="form-control" required="required" ref="task_level">Small
-			                  	<option value={1}>Medium</option>
-			                  	<option value={2}>High</option>
-			                </select>
-		              	</div>
-		              	<button type="button" className="btn btn-primary">Submit</button>
-		              	<button type="button" className="btn btn-default">Cancel</button>
-		            </form>
-	          	</div>
-        	</div>
+	        <Form />
 	        {/* FORM : END */}
 
 	        {/* LIST : START */}
-	        <div className="panel panel-success">
-	          	<div className="panel-heading">List Task</div>
-	          	<table className="table table-hover ">
-		            <thead>
-		              	<tr>
-			                <th style={{width: '10%'}} className="text-center">#</th>
-			                <th>Task</th>
-			                <th style={{width: '20%'}} className="text-center">Level</th>
-			                <th style={{width: '20%'}}>Action</th>
-		              	</tr>
-		            </thead>
-	            	<tbody>
-		              	<tr>
-			                <td className="text-center">1</td>
-			                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
-			                <td className="text-center"><span className="label label-danger">High</span></td>
-			                <td>
-			                  <button type="button" className="btn btn-warning">Edit</button>
-			                  <button type="button" className="btn btn-danger">Delete</button>
-			                </td>
-		              	</tr>
-		              	<tr>
-			                <td className="text-center">2</td>
-			                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
-			                <td className="text-center"><span className="label label-default">Small</span></td>
-			                <td>
-			                  <button type="button" className="btn btn-warning">Edit</button>
-			                  <button type="button" className="btn btn-danger">Delete</button>
-			                </td>
-		              	</tr>
-		              	<tr>
-			                <td className="text-center">3</td>
-			                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c</td>
-			                <td className="text-center"><span className="label label-info">Medium</span></td>
-			                <td>
-			                  	<button type="button" className="btn btn-warning">Edit</button>
-			                  	<button type="button" className="btn btn-danger">Delete</button>
-			                </td>
-		              	</tr>
-	            	</tbody>
-	          	</table>
-	        </div>
+	        <List/>
+	    	{/* LIST : END */}
     	</div>
 	    );
 	}
