0bdc9580f86ab0a65b3e1f61759967bda806b2d3
huynvn010193 <hpn010193@gmail.com>
2018-02-19 16:11:26 +0700

    Pending Form text 02

---
+++ study-form/src/components/StudyForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class StudyForm extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+
+		};
+	}
+	render() {
+		return (
+			<div className="row">
+				<div className="panel panel-info">
+					<div className="panel-heading">
+						<h3 className="panel-title">Study Form</h3>
+					</div>
+					<div className="panel-body">
+						<form action="#" method="POST" role="form">
+							<legend>Form Register</legend>
+							<div className="form-group">
+								<label htmlFor="true">Họ tên</label>
+								<input type="text" className="form-control" placeholder="Fullname"/>
+							</div>
+							<div className="form-group">
+								<label htmlFor="true">Khóa học</label>
+								<select name="true" id="input" className="form-control" required="required">
+									<option value="angular">Angular</option>
+									<option value="react">ReactJS</option>
+								</select>
+							</div>
+							<div className="form-group">
+								<label htmlFor="true">Đối tượng</label>
+								<div className="radio">
+									<label>
+										<input type="radio" name="dsd" id="input" defaultValue />
+									</label>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+    	);
+	}
+}
+export default StudyForm;
--- study-form/src/index.js
+++ study-form/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import StudyForm from './components/StudyForm';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<StudyForm />, document.getElementById('main-content'));
 registerServiceWorker();
