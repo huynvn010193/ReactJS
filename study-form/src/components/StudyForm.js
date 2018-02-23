49b6d9cec8d602bb366df50f706812bcd0d4023c
HuyNVN <hpn010193@gmail.com>
2018-02-23 14:31:21 +0700

    Done clip 2.9 Xây dựng chức năng Add: Form radio

--- study-form/src/components/StudyForm.js
+++ study-form/src/components/StudyForm.js
@@ -7,7 +7,8 @@ class StudyForm extends Component
 		super(props);
 		this.state = {
 			fullname:'',
-			course: 'php'
+			course: 'php',
+			object: 'nkt'
 		};
 		this.handleChange = this.handleChange.bind(this);
     	this.handleSubmit = this.handleSubmit.bind(this);
@@ -42,6 +43,7 @@ class StudyForm extends Component
 								<label htmlFor="true">Họ tên</label>
 								<input value={this.state.fullname} onChange={this.handleChange} name="fullname" type="text" className="form-control" placeholder="Fullname"/>
 							</div>
+
 							<div className="form-group">
 								<label htmlFor="true">Khóa học</label>
 								<select value={this.state.course} onChange={this.handleChange} name="course" id="input" className="form-control" required="required">
@@ -51,23 +53,24 @@ class StudyForm extends Component
 									<option value="php">PHP</option>
 								</select>
 							</div>
+
 							<div className="form-group">
 								<label htmlFor="true">Đối tượng</label>
 								<div className="radio">
 									<label>
-										<input type="radio" name="dsd" id="input" defaultValue />
+										<input onChange={this.handleChange} checked={this.state.object === 'old_student'} value="old_student" type="radio" name="object"  />
 										Học viên cũ
 									</label>
 								</div>
 								<div className="radio">
 									<label>
-										<input type="radio" name="dsd" id="input" defaultValue />
+										<input onChange={this.handleChange} checked={this.state.object === 'student'} value="student" type="radio"  name="object"   />
 										Học sinh sinh viên
 									</label>
 								</div>
 								<div className="radio">
 									<label>
-										<input type="radio" name="dsd" id="input" defaultValue />
+										<input onChange={this.handleChange} checked={this.state.object === 'nkt'} value="nkt" type="radio" name="object" />
 										Người khuyết tật
 									</label>
 								</div>
