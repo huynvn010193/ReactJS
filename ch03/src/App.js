45e00c60e80be4891970daf96b36b2ea1325c01f
huynvn010193 <hpn010193@gmail.com>
2018-03-11 21:45:12 +0700

    Pending clip htmlToJsx

--- ch03/src/App.js
+++ ch03/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import {CopyToClip}
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const HTMLtoJSX = require('@tsuyoshiwada/htmltojsx');
 class App extends Component {
-    constructor(props)
-    {
-        super(props);
-    }
-
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			input : 
+			`<div class="foo bar" style="width:400px; padding: 20px 5px; background:white;">
+				<h2 data-value="foo">Support SVG attributes</h2>
+			</div>`
+		}
+	}
     render() {
-        return (
-            
-        );
+    	return (
+    		<div className="row">Hello 2</div>
+    	)
     }
 }
 export default App;
