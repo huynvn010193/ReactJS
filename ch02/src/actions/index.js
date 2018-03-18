335933cc88442fad21d9d63e9a928d4480241837
huynvn010193 <hpn010193@gmail.com>
2018-03-18 14:54:55 +0700

    Done clip 3.3:Su dung redux_Redux 03

--- ch02/src/actions/index.js
+++ ch02/src/actions/index.js
@@ -1,20 +1,30 @@
 export const actCloseForm = () => 
 {
 	return {
-		'type' : 'CLOSE_FORM'
+		type : 'CLOSE_FORM'
 	}
 }
 
 export const actOpenForm = () => 
 {
 	return {
-		'type' : 'OPEN_FORM'
+		type : 'OPEN_FORM'
 	}
 }
 
 export const actToggleForm = () => 
 {
 	return {
-		'type' : 'TOGGLE_FORM'
+		type : 'TOGGLE_FORM'
+	}
+}
+
+export const actSort = (orderBy,orderDir) => 
+{
+	return {
+		type: 'SORT_ITEM',
+		orderBy : orderBy,
+		orderDir: orderDir
+		
 	}
 }
--- ch02/src/redux.js
+++ ch02/src/redux.js
@@ -1,34 +1,7 @@
 import { createStore } from 'redux'
-import { actCloseForm,actToggleForm,actOpenForm } from './actions/index'
+import appReducers  from './reducers/index'
+import { actCloseForm,actToggleForm,actOpenForm,actSort } from './actions/index'
 
-let defaultState = {
-	items : [],
-	iShowForm : true,
-	strSearch:'',
-	orderBy: 'name',
-	orderDir: 'asc',
-	itemSelected: null
-}
-
-let appReducers = (state = defaultState,action) => {
-	switch(action.type)
-	{
-		case 'CLOSE_FORM':
-			//console.log("CLOSE_FORM",state);
-			state.iShowForm = false;
-			return state;
-		case 'OPEN_FORM':
-			//console.log("CLOSE_FORM",state);
-			state.iShowForm = true;
-			return state;
-		case 'TOGGLE_FORM':
-			//console.log("CLOSE_FORM",state);
-			state.iShowForm = !state.iShowForm;
-			return state;
-		default:
-			return state;
-	}
-}
 const  store = createStore(appReducers);
 console.log('Init: ', store.getState());
 
@@ -44,7 +17,10 @@ console.log('OPEN_FORM: ', store.getState());
 store.dispatch(actToggleForm());
 console.log('TOGGLE_FORM: ', store.getState());
 
-export default store;
+// CHANGE_SORT
+store.dispatch(actSort("level","desc"));
+console.log('SORT_ITEM: ', store.getState());
 
+export default store;
 // store chứa state. Lấy state => getState
 // action -> store - > appReducers.
