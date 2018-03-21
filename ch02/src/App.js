8dd777e62101cda7afa55802a606946cc34f37db
huynvn010193 <hpn010193@gmail.com>
2018-03-21 23:37:19 +0700

    Pending clip 3.4: Hiện thị danh sách Item với Redux_Part01

--- ch02/src/App.js
+++ ch02/src/App.js
@@ -3,7 +3,7 @@ import Title from './components/Title';
 import Control from './components/Control';
 import Form from './components/Form';
 import List from './components/List';
-import store from './redux';
+
 //import tasks from './mocks/tasks'
 import {filter,includes, orderBy as funcOrderBy, remove, reject } from 'lodash';
 const uuidv4 = require('uuid/v4');
--- ch02/src/index.js
+++ ch02/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //import Hello from './components/Hello';
-
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-	<App />,
+	<Provider store={ store }>
+		<App />
+	</Provider> ,
 	 document.getElementById('main-content'));
 registerServiceWorker();
