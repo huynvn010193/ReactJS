e7a8b478b13013678050d1e8f6749a0cacc4d962
huynvn010193 <hpn010193@gmail.com>
2018-04-15 19:54:38 +0700

    Done clip 4.3: Xạy dựng chức năng hiển thị sản phẩm: reducers

---
+++ ch04/src/reducers/carts.js
@@ -0,0 +1,24 @@
+import * as types from './../constants/ActionType';
+import * as configs from './../constants/Config';
+
+// danh sách sản phẩm nằm trong giỏ hàng => phải là 1 array
+let defaultState = [];
+let cardItems = JSON.parse(localStorage.getItem(configs.CARTS_FROM_LOCAL_STOGARE));
+defaultState = (cardItems !== null && cardItems.length > 0) 
+
+const carts = (state = defaultState, action) => {
+	switch(action.type)
+	{
+		case types.BUY_PRODUCT:
+			return state;
+		case types.UPDATE_PRODUCT:
+			return state;
+		case types.REMOVE_PRODUCT:
+			return state;	
+		default:
+			return state;
+	}
+}
+
+export default carts;
+
---
+++ ch04/src/reducers/products.js
@@ -0,0 +1,50 @@
+import * as types from './../constants/ActionType';
+import * as configs from './../constants/Config';
+
+let defaultState = [
+	{
+		id: 'aplusautomation123',
+		name: 'aplus automation',
+		image: 'aplusautomation.jpg',
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta asperiores veniam repellat unde debitis quisquam magnam magni ut deleniti!',
+		price: 12,
+		canBuy: true
+	},
+	{
+		id: 'aplusmedia',
+		name: 'aplus media',
+		image: 'aplus-media.jpg',
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta asperiores veniam repellat unde debitis quisquam magnam magni ut deleniti!',
+		price: 22,
+		canBuy: false
+	},
+	{
+		id: 'robofigcombo',
+		name: 'robo fig combo',
+		image: 'robo_fig_combo.jpg',
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta asperiores veniam repellat unde debitis quisquam magnam magni ut deleniti!',
+		price: 5,
+		canBuy: true
+	},
+	{
+		id: 'targetleapfrog',
+		name: 'target leap frog',
+		image: 'target-leap-frog.jpg',
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta asperiores veniam repellat unde debitis quisquam magnam magni ut deleniti!',
+		price: 5,
+		canBuy: true
+	}
+
+];
+const products = (state = defaultState, action) => {
+	switch(action.type)
+	{
+		case types.LIST_PRODUCT:
+			return state;
+		default:
+			return state;
+	}
+}
+
+export default products;
+
