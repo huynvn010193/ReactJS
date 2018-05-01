e744def756aedace7a4acc048e36020af1523ef7
huynvn010193 <hpn010193@gmail.com>
2018-05-01 09:12:10 +0700

    Done clip 5.2: Sử dựng React Router: 4 - menu config 01

--- ch05/src/components/Menu.js
+++ ch05/src/components/Menu.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+
+// const dùng định nghĩa những giá trị menu có thể có:
+const menus = [
+	{to: '/', exact: true, name:'Home'},
+	{to: '/about', exact: false, name:'About'},
+	{to: '/course', exact: false, name:'Course'}
+]
+
 class Menu extends Component{
 	render(){
 		return(
 			<div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 				<div className="list-group">
-					<NavLink exact to="/" activeClassName="active" className="list-group-item">Home</NavLink>
-					<NavLink to="/About" activeClassName="active" className="list-group-item">About</NavLink>
-					<NavLink to="/Course" activeClassName="active" className="list-group-item">Course</NavLink>
+					{this.showMenu(menus)}
 				</div>
 			</div>
 		);
+	}
+
+	showMenu(menus)
+	{
+		let xhtml = null;
+		if(menus.length > 0)
+		{
+			xhtml = menus.map((menu,index)=>{
+				return (<NavLink key={index} exact={menu.exact} to={menu.to} activeClassName="active" className="list-group-item">
+						{menu.name}
+					</NavLink>
+				);
+			});
+		}
+		return xhtml;
 	}
 }
 
