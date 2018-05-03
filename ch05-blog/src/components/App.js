c9686f803dff55f0b152a56913585771ad7d7c54
huynvn010193 <hpn010193@gmail.com>
2018-05-04 00:22:19 +0700

    Done clip 5.3: Xây dựng cấu trúc ứng dụng: 2 - Component 01

--- ch05-blog/src/components/App.js
+++ ch05-blog/src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Slider from "./Slider";
+import NewsItem from "./NewsItem";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-        
+        	<Header />
+        	<Navigation />
+        	<div className="container">
+        		<Slider />
+        		<NewsItem />
+        		<NewsItem />
+        		<NewsItem />
+        	</div>
+        	<Footer/>
         </div>
     );
   }
