47fe5b23ade5fdf7068a885bdebe390bae260f1d
huynvn010193 <hpn010193@gmail.com>
2018-05-06 12:24:50 +0700

    Done clip 5.5: Xây dựng Menu và Custom Link

--- ch05-blog/src/components/FormLogin.js
+++ ch05-blog/src/components/FormLogin.js
@@ -11,23 +11,15 @@ class BlogItem extends Component {
 			<hr className="divider" />
 			<form>
 			    <div className="row">
-			        <div className="form-group col-lg-4">
-			            <label className="text-heading">Name</label>
+			        <div className="form-group col-lg-6">
+			            <label className="text-heading">User name</label>
 			            <input type="text" className="form-control" />
 			        </div>
-			        <div className="form-group col-lg-4">
-			            <label className="text-heading">Email Address</label>
-			            <input type="email" className="form-control" />
-			        </div>
-			        <div className="form-group col-lg-4">
-			            <label className="text-heading">Phone Number</label>
-			            <input type="tel" className="form-control" />
+			        <div className="form-group col-lg-6">
+			            <label className="text-heading">Password</label>
+			            <input type="password" className="form-control" />
 			        </div>
 			        <div className="clearfix" />
-			            <div className="form-group col-lg-12">
-			                <label className="text-heading">Message</label>
-			                <textarea className="form-control" rows={6} defaultValue={""} />
-			            </div>
 			            <div className="form-group col-lg-12">
 			                <button type="submit" className="btn btn-secondary">Submit</button>
 			            </div>
--- ch05-blog/src/components/Navigation.js
+++ ch05-blog/src/components/Navigation.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const menus = [
+	{to: '/', exact: true, name:'Home'},
+	{to: '/about', exact: false, name:'About'},
+	{to: '/blog', exact: false, name:'Blog'},
+	{to: '/login', exact: true, name:'Login'},
+]
 
 class Navigation extends Component {
   render() {
     return (
     	<nav className="navbar navbar-expand-lg navbar-light bg-faded py-lg-4">
 		    <div className="container">
-		        <a className="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="http://localhost:3000/">Start Bootstrap</a>
+		    	<NavLink to="/" className="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none">
+					Start Bootstrap
+				</NavLink>
 		        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		        <span className="navbar-toggler-icon" />
 		        </button>
 		        <div className="collapse navbar-collapse" id="navbarResponsive">
 		            <ul className="navbar-nav mx-auto">
-		                <li className="nav-item active px-lg-4">
-		                    <a className="nav-link text-uppercase text-expanded" href="index.html">Home
-		                    <span className="sr-only">(current)</span>
-		                    </a>
-		                </li>
-		                <li className="nav-item px-lg-4">
-		                    <a className="nav-link text-uppercase text-expanded" href="about.html">About</a>
-		                </li>
-		                <li className="nav-item px-lg-4">
-		                    <a className="nav-link text-uppercase text-expanded" href="blog.html">Blog</a>
-		                </li>
-		                <li className="nav-item px-lg-4">
-		                    <a className="nav-link text-uppercase text-expanded" href="contact.html">Contact</a>
-		                </li>
+		            	{this.showMenu(menus)}
 		            </ul>
 		        </div>
 		    </div>
 		</nav>
 	);
   	}
+
+  	showMenu(menus)
+	{
+		let xhtml = null;
+		if(menus.length > 0)
+		{
+			xhtml = menus.map((menu,index)=>{
+				return (
+					<li key={index} className="nav-item px-lg-4">
+						<NavLink exact={menu.exact} to={menu.to} activeClassName="active" className="nav-link text-uppercase text-expanded">
+							{menu.name}
+						</NavLink>
+					</li>
+				);
+			});
+		}
+		return xhtml;
+	}
 }
 
 export default Navigation;
