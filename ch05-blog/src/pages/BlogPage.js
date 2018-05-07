368fe77bdb78f4af6a0b887431648de9d1f2ea03
huynvn010193 <hpn010193@gmail.com>
2018-05-07 07:23:50 +0700

    Done clip 5.6: Xây dựng Page Login giới hạn vùng truy cập: 2_BlogPage

--- ch05-blog/src/pages/BlogPage.js
+++ ch05-blog/src/pages/BlogPage.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import BlogItem from "./../components/BlogItem";
 import NewsItem from "./../components/NewsItem";
+import { Redirect } from 'react-router-dom'
 
 class BlogPage extends Component {
-  render() {
-    return (
-    	<div>
-    		<BlogItem />	
-    		<NewsItem />
-    	</div>
-	);
+  	render() {
+	  	let {user} = this.props;
+	  	if(user.isLogin === false)
+	  	{
+	  		return <Redirect to="/login" />;
+	  	}
+	    return (
+	    	<div>
+	    		<BlogItem />	
+	    		<NewsItem />
+	    	</div>
+		);
   	}
 }
 
-export default BlogPage;
+const mapStateToProps = state =>
+{
+	return {
+		user: state.user
+	}
+}
+export default connect(mapStateToProps,null)(BlogPage);
