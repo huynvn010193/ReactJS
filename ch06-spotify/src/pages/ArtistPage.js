0ff1b32d250cd52a5d66e9661d8048451d323898
huynvn010193 <hpn010193@gmail.com>
2018-06-02 21:12:57 +0700

    Done clip 6.6: Xây dựng trang Artist: 02_showArtist

--- ch06-spotify/src/pages/ArtistPage.js
+++ ch06-spotify/src/pages/ArtistPage.js
@@ -28,20 +28,29 @@ class ArtistPage extends Component
     }
 
     render(){
-        console.log(this.state.artist);
+        let artist = {
+            name : '',
+            external_urls : '',
+            genres: [],
+            images: [{url:''}]
+        }
+        artist = this.state.artist !== null ? this.state.artist : artist;
         return (
             <div className="panel panel-info">
                 <div className="panel-heading">
-                    <h3 className="panel-title">Singer</h3>
+                    <h3 className="panel-title">{artist.name}</h3>
                 </div>
                 <div className="panel-body">
                     <div className="panel-body">
                         <div className="col-sm-4 col-md-4">
-                            <img src="https://i.scdn.co/image/23b3caeb7f6d181787bd5bd04f5c0ddbc332dcab" alt="Thu Minh" className="img-rounded img-responsive" />
+                            {this.showImage(artist)}
                             <blockquote style={{marginTop: 20}}>
-                            <p>Thu Minh</p>
+                            <p>{artist.name}</p>
                             </blockquote>
-                            <p><i className="glyphicon glyphicon-play-circle" /><a rel="noopener noreferrer" target="_blank" href="https://open.spotify.com/artist/4mzMFxVZNS2uCVNdsVFoj5"> View Spotify</a><br /><br /><i className="glyphicon glyphicon-play-circle" /> Genres:<span className="label label-warning" style={{marginRight: 5}}>vietnamese pop</span></p>
+                            <p><i className="glyphicon glyphicon-play-circle" /><a rel="noopener noreferrer" target="_blank" href={artist.external_urls.spotify}> View Spotify</a><br /><br />
+                                <i className="glyphicon glyphicon-play-circle" /> 
+                                Genres: {this.showGenres(artist.genres)}
+                            </p>
                         </div>
                         <div className="col-sm-8 col-md-8">
                             <div className="panel panel-danger">
@@ -58,7 +67,33 @@ class ArtistPage extends Component
                     </div>
                 </div>
             </div>
-        )
+        );
+    }
+
+    showGenres(genres)
+    {
+        let xhtml = null;
+        if(genres !== null && genres.length > 0)
+        {
+            xhtml = genres.map((genre,index) => {
+                return (
+                    <span key={index} className="label label-warning" style={{marginRight: 5}}>
+                        {genre}
+                    </span>
+                );
+            });
+        }
+        return xhtml;
+    }
+
+    showImage(item)
+    {
+        let xhtml = null;
+        if(item.images !== null && item.images.length > 1)
+        {
+            xhtml = <img src={item.images[0].url} alt={item.name} className="img-rounded img-responsive"/>
+        }
+        return xhtml;
     }
 }
 
