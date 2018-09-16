179484c6789104d0e428bf661e0e6c961885d6a4
huynvn010193 <hpn010193@gmail.com>
2018-09-16 22:57:00 +0700

    Fix done error load page, for router config

--- ch07-task-manager/src/route-config.js
+++ ch07-task-manager/src/route-config.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import NotFoundPage from './pages/NotFoundPage';
+// import NotFoundPage from './pages/NotFoundPage';
 import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
-import TaskPage from './pages/TaskPage';
-import UserPage from './pages/UserPage';
+// import SignUpPage from './pages/SignUpPage';
+// import TaskPage from './pages/TaskPage';
+// import UserPage from './pages/UserPage';
 
 const routers = [
+    // {
+    //     path:  "/",
+    //     exact: true,
+    //     main:() => <TaskPage />
+    // },
+    // {
+    //     path: "/user",
+    //     exact: true,
+    //     main:() => <UserPage />
+    // },
     {
-        path:  "/",
-        exact: true,
-        main:() => <TaskPage />
-    },
-    {
-        path: "/user",
-        exact: true,
-        main:() => <UserPage />
-    },
-    {
-        path: "/singin",
+        path: "/",
         exact: true,
         main:() => <SignInPage />
     },
-    {
-        path: "/singup",
-        exact: true,
-        main:() => <SignUpPage />
-    },
-    {
-        path: "/task",
-        exact: true,
-        main:() => <SignUpPage />
-    },
-    {
-        path: "",
-        exact: true,
-        main:() => <NotFoundPage />
-    }
+    // {
+    //     path: "/singup",
+    //     exact: true,
+    //     main:() => <SignUpPage />
+    // },
+    // {
+    //     path: "/task",
+    //     exact: true,
+    //     main:() => <SignUpPage />
+    // },
+    // {
+    //     path: "",
+    //     exact: true,
+    //     main:() => <NotFoundPage />
+    // }
 ]
 
 export default routers;
