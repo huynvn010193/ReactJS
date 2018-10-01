619f916b6db123f81134397c63bbbaa421f95191
huynvn010193 <hpn010193@gmail.com>
2018-10-01 22:05:25 +0700

    Done clip 7.4: Xay dung Task Page-Task Doing: 03-List Item

--- ch07-task-manager/src/components/TaskDoingList.js
+++ ch07-task-manager/src/components/TaskDoingList.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import TaskDoingItem from './TaskDoingItem';
 import FormAddTask from './FormAddTask';
+import { taskRef } from './../firebase';
 
 class TaskDoingList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    }
+  }
+
+  // Trước khi bắt đầu render() => đổ dữ liệu vào
+  componentWillMount(){
+    taskRef.on('value', items => {
+      let data = [];
+      items.forEach(item =>{
+        let { email,name } = item.val();
+        data.push({ email,name });
+      })
+      this.setState({ items: data});
+    });
+  }
+
   render() {
+    let { items } = this.state;
+    console.log(items);
+
     return(
       <div className="panel panel-info">
         <div className="panel-heading">
           <h3 className="panel-title">Task Doing</h3></div>
         <div className="panel-body">
           <ul className="list-group">
+            <TaskDoingItem />
             <TaskDoingItem />
             <TaskDoingItem />
           </ul>
