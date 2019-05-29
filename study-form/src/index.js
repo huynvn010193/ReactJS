1412c647f7335406c1d93af0c169d4867d5e34eb
huynvn010193 <hpn010193@gmail.com>
2019-05-29 07:51:53 +0700

    Clear source

--- study-form/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StudyForm from './components/StudyForm';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<StudyForm />, document.getElementById('main-content'));
-registerServiceWorker();
