fd25f85afc219bf8de8276335f74ab4b29d4f22f
huynvn <hpn010193@gmail.com>
2020-03-29 23:50:25 +0700

    Adding top bar

--- medium-clone/src/index.js
+++ medium-clone/src/index.js
@@ -6,15 +6,14 @@ import * as serviceWorker from './serviceWorker'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
+import Topbar from 'components/topbar'
 
 const App = () => {
   return (
-    <div className="App">
-      <h3>Welcome here is our app</h3>
-      <Router>
-        <Routes />
-      </Router>
-    </div>
+    <Router>
+      <Topbar />
+      <Routes />
+    </Router>
   )
 }
 
@@ -25,7 +24,4 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
