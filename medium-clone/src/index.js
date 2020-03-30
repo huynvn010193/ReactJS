cad38d8ef97ffb1a5231e1d9b81e6876962cd9e7
huynvn <hpn010193@gmail.com>
2020-03-30 15:14:08 +0700

    11.UseRef

--- medium-clone/src/index.js
+++ medium-clone/src/index.js
@@ -17,11 +17,6 @@ const App = () => {
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.unregister()
