b197ff55c47939b81ec794ebb5d4ecf5287514f6
huynvn <hpn010193@gmail.com>
2020-05-01 00:13:47 +0700

    108. Redux-Saga: Tích Hợp Material-UI

--- react-antd-layout/src/Cricketer.js
+++ react-antd-layout/src/Cricketer.js
@@ -1,64 +1,66 @@
 import React from 'react';
+import { Card, Avatar } from 'antd';
+import Meta
 
-class Cricketer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: props.name,
-      team: props.team
-    }
-  }
+// class Cricketer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: props.name,
+//       team: props.team
+//     }
+//   }
 
-  getName = () => this.state.name;
+//   getName = () => this.state.name;
 
-  render() {
-    return (
-      <div>
-        <span> Team: {this.state.team}</span>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <span> Team: {this.state.team}</span>
+//       </div>
+//     )
+//   }
+// }
 
-export class Bowler extends Cricketer {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.state,
-      wickets: props.wickets
-    }
-  }
+// export class Bowler extends Cricketer {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       ...this.state,
+//       wickets: props.wickets
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        <span>name: {this.getName()}</span>
-        {super.render()}
-        <span>Wickets: {this.state.wickets}</span>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <span>name: {this.getName()}</span>
+//         {super.render()}
+//         <span>Wickets: {this.state.wickets}</span>
+//       </div>
+//     )
+//   }
+// }
 
-export class Batsmen extends Cricketer {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.state,
-      runs: props.runs
-    }
-  }
+// export class Batsmen extends Cricketer {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       ...this.state,
+//       runs: props.runs
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        <span>name: {this.getName()}</span>
-        {super.render()}
-        <span>Runs: {this.state.runs}</span>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <span>name: {this.getName()}</span>
+//         {super.render()}
+//         <span>Runs: {this.state.runs}</span>
+//       </div>
+//     )
+//   }
+// }
 
 // class Cricketer {
 //   constructor(name, team) {
@@ -89,3 +91,6 @@ export class Batsmen extends Cricketer {
 
 //   getRuns = () => this.runs;
 // }
+
+
+
